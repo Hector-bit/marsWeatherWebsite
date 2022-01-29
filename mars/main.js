05fc13320a8ef40839e1dd7a497716c816bf0608
Hector-bit <hectormartinez.2978@gmail.com>
2022-01-29 03:54:29 -0800

    animation and sphere added

--- mars/main.js
+++ mars/main.js
@@ -4,19 +4,44 @@ import * as THREE from 'three';
 
 const scene = new THREE.Scene();
 
-const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window. innerHeight, 0.1, 1000)
+const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000)
 
 const renderer = new THREE.WebGLRenderer({
-  canvas: document.querySelector('#bg')
+  canvas: document.querySelector('#bg'),
 })
 
-renderer.setPixelRatio( window.devicePixelRation );
-renderer.setSize( windowwww.innerWWwidth, window.innerHeight )
+renderer.setPixelRatio( window.devicePixelRatio );
+renderer.setSize( window.innerWidth, window.innerHeight )
 camera.position.setZ(30);
 
 renderer.render( scene, camera );
 
 //objects
+const geometry = new THREE.SphereGeometry(10, 40, 18);
+const material = new THREE.MeshStandardMaterial( { color: 0xFF6347, wireFrame: true });
+const sphere = new THREE.Mesh( geometry, material );
 
-const geometry = new THREE.Sphere(10, 40, 18)
+//scene
+scene.add(sphere)
+
+const pointLight = new THREE.PointLight(0xffffff)
+
+scene.add(pointLight)
+pointLight.position.set(30,30,30)
+
+
+
+//animations
+function animate() {
+  requestAnimationFrame( animate )
+
+  sphere.rotation.x += 0.01;
+  sphere.rotation.y += 0.005;
+  sphere.rotation.z += 0.01;
+
+  renderer.render( scene, camera );
+}
+
+animate()
+
 
