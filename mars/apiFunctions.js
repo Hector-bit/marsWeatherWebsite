e17b8a95bfd7b59cee1ee94bc305284bc4909682
Hector-bit <hectormartinez.2978@gmail.com>
2022-01-30 09:51:25 -0800

    data and planet show up

--- mars/apiFunctions.js
+++ mars/apiFunctions.js
@@ -1,25 +1,15 @@
-const axios = require('axios')
-API_KEY = process.env.API_KEY;
+import axios from 'axios';
 
-function parseMarsData(json){
-
-}
-
-function get_mars_weather(){
+export const get_mars_weather = () => {
     axios
         .get("https://api.maas2.apollorion.com/")
         .then((res) => {
-            console.log(res, 'heres the response')
-            return res
+            console.log(res.data, 'heres the response')
+            return res.data
         })
         .catch((err) => {
             console.log('there was an issue getting response from nasa website', err)
             return err
         })
 }
-
-
-get_mars_weather();
-
-
 
--- mars/main.js
+++ mars/main.js
@@ -1,9 +1,11 @@
 import './style.css'
 import * as THREE from 'three';
-
+import { get_mars_weather } from './apiFunctions';
 
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000)
+const marsData = get_mars_weather();
+console.log(marsData)
 
 const renderer = new THREE.WebGLRenderer({
   canvas: document.querySelector('#bg'),
