8c99ddea33aaabcd35e0b8eafe1916e5554d4612
Hector-bit <hectormartinez.2978@gmail.com>
2022-02-02 02:02:02 -0800

    test 2

--- mars/main.js
+++ mars/main.js
@@ -1,5 +1,5 @@
 import './style.css'
-import * as THREE from 'three';
+import * as THREE from './node_modules/three';
 import { OrbitControls } from './node_modules/three/examples/jsm/controls/OrbitControls';
 import backgroundPicure from '/space.jpg';
 import marsSurface from '/mars.jpg'
