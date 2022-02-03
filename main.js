5caf9f69d94b284b950520b22d910548977df0bb
Hector-bit <hectormartinez.2978@gmail.com>
2022-02-02 17:42:51 -0800

    new folder structure

--- main.js
+++ main.js
@@ -1,8 +1,8 @@
-import './style.css'
+import './css/style.css';
 import * as THREE from './node_modules/three';
 import { OrbitControls } from './node_modules/three/examples/jsm/controls/OrbitControls.js';
-import backgroundPicure from '/space.jpg';
-import marsSurface from '/mars.jpg'
+import backgroundPicure from './pictures/space.jpg';
+import marsSurface from './pictures/mars.jpg'
 
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
